188c74ddb2f592f344b6428f88f00017df1dd82c
Giulia Martinelli <62147733+giuli13@users.noreply.github.com>
2025-02-25 13:59:20 +0100

    Update shape_optimization.py

--- shape_optimization.py
+++ shape_optimization.py
@@ -279,17 +279,17 @@ def main():
 
     batch_size = 1
     device = torch.device('cuda')
-    mesh_path = "./datasets/mixamo/training_shape"
+    mesh_path = "./dataset/training_shape"
     character_name = "BigVegas"
-    bvh_path = "/home/giuliamartinelli/Dataset/MoMaAnimation/MixamoBVH"
+    bvh_path = "./demo_file/MixamoBVH"
     if '_m' in character_name:
         char_name = character_name.split('_m')[0]
     else:
         char_name = character_name
 
-    t_pose_path = "/home/giuliamartinelli/Dataset/MoMaAnimation/Tpose/%s.npy" %(char_name)
+    t_pose_path = "./dataset/Tpose/%s.npy" %(char_name)
     file_names = os.listdir(mesh_path)
-    json_file_path = './datasets/filelist/image/image.json'
+    json_file_path = './demo_file/demo.json'
 
     ## Load Tpose
     t_pose = torch.tensor(np.load(t_pose_path)).cuda().float()
